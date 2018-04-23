a4e597cc7802d19baaf5cd10b8bf957517062f6e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/XtraPivotGrid_CreateDrillDownDataSourceAsync/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace XtraPivotGrid_CreateDrillDownDataSourceAsync {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.salesPersonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nwindDataSet = new XtraPivotGrid_CreateDrillDownDataSourceAsync.nwindDataSet();
+            this.fieldCountry1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldProductName1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldCategoryName1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldExtendedPrice1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldSalesPerson1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.salesPersonTableAdapter = new XtraPivotGrid_CreateDrillDownDataSourceAsync.nwindDataSetTableAdapters.SalesPersonTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesPersonBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pivotGridControl1
+            // 
+            this.pivotGridControl1.DataSource = this.salesPersonBindingSource;
+            this.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pivotGridControl1.Fields.AddRange(new DevExpress.XtraPivotGrid.PivotGridField[] {
+            this.fieldCountry1,
+            this.fieldProductName1,
+            this.fieldCategoryName1,
+            this.fieldExtendedPrice1,
+            this.fieldSalesPerson1});
+            this.pivotGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.Size = new System.Drawing.Size(675, 329);
+            this.pivotGridControl1.TabIndex = 0;
+            this.pivotGridControl1.CellClick += new DevExpress.XtraPivotGrid.PivotCellEventHandler(this.pivotGridControl1_CellClick);
+            // 
+            // salesPersonBindingSource
+            // 
+            this.salesPersonBindingSource.DataMember = "SalesPerson";
+            this.salesPersonBindingSource.DataSource = this.nwindDataSet;
+            // 
+            // nwindDataSet
+            // 
+            this.nwindDataSet.DataSetName = "nwindDataSet";
+            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // fieldCountry1
+            // 
+            this.fieldCountry1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.fieldCountry1.AreaIndex = 0;
+            this.fieldCountry1.Caption = "Country";
+            this.fieldCountry1.FieldName = "Country";
+            this.fieldCountry1.Name = "fieldCountry1";
+            // 
+            // fieldProductName1
+            // 
+            this.fieldProductName1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.fieldProductName1.AreaIndex = 1;
+            this.fieldProductName1.Caption = "Product Name";
+            this.fieldProductName1.FieldName = "ProductName";
+            this.fieldProductName1.Name = "fieldProductName1";
+            // 
+            // fieldCategoryName1
+            // 
+            this.fieldCategoryName1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.fieldCategoryName1.AreaIndex = 0;
+            this.fieldCategoryName1.Caption = "Category Name";
+            this.fieldCategoryName1.FieldName = "CategoryName";
+            this.fieldCategoryName1.Name = "fieldCategoryName1";
+            // 
+            // fieldExtendedPrice1
+            // 
+            this.fieldExtendedPrice1.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
+            this.fieldExtendedPrice1.AreaIndex = 0;
+            this.fieldExtendedPrice1.Caption = "Extended Price";
+            this.fieldExtendedPrice1.FieldName = "Extended Price";
+            this.fieldExtendedPrice1.Name = "fieldExtendedPrice1";
+            // 
+            // fieldSalesPerson1
+            // 
+            this.fieldSalesPerson1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.fieldSalesPerson1.AreaIndex = 1;
+            this.fieldSalesPerson1.Caption = "Sales Person";
+            this.fieldSalesPerson1.FieldName = "Sales Person";
+            this.fieldSalesPerson1.Name = "fieldSalesPerson1";
+            // 
+            // salesPersonTableAdapter
+            // 
+            this.salesPersonTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(226)))), ((int)(((byte)(231)))));
+            this.label1.Location = new System.Drawing.Point(201, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RowCount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 329);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pivotGridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesPersonBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private nwindDataSet nwindDataSet;
+        private System.Windows.Forms.BindingSource salesPersonBindingSource;
+        private XtraPivotGrid_CreateDrillDownDataSourceAsync.nwindDataSetTableAdapters.SalesPersonTableAdapter salesPersonTableAdapter;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldCountry1;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldProductName1;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldCategoryName1;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldExtendedPrice1;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldSalesPerson1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to use asynchronous operations that return the result
+
+
+<p>This example shows how to use asynchronous operations that return the result. <br />
+In this example, the <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridPivotGridControl_CellClicktopic"><u>PivotGridControl.CellClick</u></a> event is handled to generate a drill-down data source for a specific cell using the <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridPivotGridControl_CreateDrillDownDataSourceAsynctopic"><u>PivotGridControl.CreateDrillDownDataSourceAsync</u></a> method. The <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridAsyncCompletedHandlertopic"><u>AsyncCompletedHandler</u></a> delegate is passed as a method parameter. The referenced method is executed after the operation is completed, allowing to obtain the result via the <i>result</i> parameter of the delegate. This parameter returns an <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridAsyncOperationResultMembersTopicAll"><u>AsyncOperationResult</u></a> instance.<br />
+The <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridAsyncOperationResult_Valuetopic"><u>AsyncOperationResult.Value</u></a> property is used to obtain a record set associated with the clicked cell. Then, the <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridPivotDrillDownDataSource_RowCounttopic"><u>PivotDrillDownDataSource.RowCount</u></a> property is used to determine the number of records in the data source for this cell.</p>
+
+<br/>
+
+
